71cd71ac72d7cb6719d671ddc1992b8a531066a5
vitor <vitor.ecomp@gmail.com>
2018-08-02 19:10:35 -0300

    Adding the base class, with the basics method

---
+++ src/kong-api.js
@@ -0,0 +1,28 @@
+
+
+class KongApi {
+    constructor(entryPoint){
+        this.entryPoint = entryPoint
+
+        //init variables
+        this.consumers = []
+        this.services = []
+        this.plugins = []
+    }
+
+
+    getServices(limit){}
+    getService(id){
+        return id
+    }
+    addService(service){}
+
+    getConsumers(){}
+    addService(){}
+
+    getServices(){}
+    syncServices(){}
+    deleteService(){}
+
+    
+}
