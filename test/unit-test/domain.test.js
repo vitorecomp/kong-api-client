eddd6cae22ab33bca6f8119757f2e120d8fd0bda
vitor.ecomp <vitor.ecomp@gmail.com>
2020-01-28 11:52:40 -0300

    Start test build

---
+++ test/unit-test/domain.test.js
@@ -0,0 +1,16 @@
+
+test('should setup remove expense action object', () => {
+
+});
+
+test('should setup the edit expense action object', () => {
+
+});
+
+test('should setup the add expense action object', () => {
+
+});
+
+test('should setup the add expense action object with default', () => {
+
+});
