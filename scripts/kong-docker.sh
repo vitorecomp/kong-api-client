ab84f25d898f0c73173e2e1fcd23674280a4782f
vitorecomp <vitor.ecomp@gmail.com>
2023-08-01 18:07:35 -0300

    feat: change the action to create docker

--- scripts/kong-docker.sh
+++ scripts/kong-docker.sh
@@ -8,12 +8,14 @@ docker run -d --name kong-database \
                 -e "POSTGRES_USER=kong" \
                 -e "POSTGRES_DB=kong" \
                 postgres:9.6
+
 docker run --rm \
     --link kong-database:kong-database \
     -e "KONG_DATABASE=postgres" \
     -e "KONG_PG_HOST=kong-database" \
     -e "KONG_CASSANDRA_CONTACT_POINTS=kong-database" \
     kong kong migrations bootstrap
+
 docker run -d --name kong \
     --link kong-database:kong-database \
     -e "KONG_DATABASE=postgres" \
