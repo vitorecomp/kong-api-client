e22f41c5a14d8e5ae5680b6e253f3a886ecfa547
vitorecomp <vitor.ecomp@gmail.com>
2023-08-01 17:10:17 -0300

    feat: test the ci/cd

---
+++ scripts/run-tests.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+#get the current directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+echo "Runnnig the tests"
+
+#if arg 1 is podman
+if [ "$1" == "podman" ]; then
+    echo "Running podman containers"
+    sh $DIR/kong-podman.sh start --clean
+    echo "Running the tests"
+    npm run tests
+    echo "stoping the containers"
+    sh $DIR/kong-podman.sh stop
+    exit 0
+else
+    echo "Running docker containers"
+    sh $DIR/kong-docker.sh start --clean
+    npm run tests
+    echo "stoping the containers"
+    sh $DIR/kong-docker.sh stop
+    exit 0
+fi
