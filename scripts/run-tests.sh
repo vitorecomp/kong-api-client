4557eeeb9a745a83cd8f43318c65788396a1b401
vitorecomp <vitor.ecomp@gmail.com>
2023-08-01 17:23:11 -0300

    feat: change the badge, add env

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -11,14 +11,14 @@ if [ "$1" == "podman" ]; then
     echo "Running podman containers"
     sh $DIR/kong-podman.sh start --clean
     echo "Running the tests"
-    npm run tests
+    npm run test
     echo "stoping the containers"
     sh $DIR/kong-podman.sh stop
     exit 0
 else
     echo "Running docker containers"
     sh $DIR/kong-docker.sh start --clean
-    npm run tests
+    npm run test
     echo "stoping the containers"
     sh $DIR/kong-docker.sh stop
     exit 0
